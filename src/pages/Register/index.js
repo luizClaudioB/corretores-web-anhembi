f054f577a9763a3bb71d9970163192705a609b4b
raphaelhcc <raphaelhcc321@gmail.com>
2020-04-12 01:23:39 -0300

    mask, cpf validation, CEP api

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,6 +1,10 @@
 import React, { useState, Component, Link } from 'react';
 import './styles.css';
 import { render } from '@testing-library/react';
+import ApiCep from '../../services/ApiCep';
+import InputMask from 'react-input-mask';
+
+
 
 export default class Register extends Component {
     constructor(props) {
@@ -10,9 +14,91 @@ export default class Register extends Component {
             ischeckedB:false,
             isdisableS:false,
             isdisableB:false,
+            rua: "",
+            bairro: "",
+            localidade: "",
+            estado: "",
         }
     };
 
+    handleCPFB(e) {
+        const CPFB=e.target.value
+        {console.log(CPFB)}
+        var Soma;
+        var Resto;
+        Soma = 0;
+        if (CPFB == "00000000000") return false;
+         
+        for (let i=1; i<=9; i++) Soma = Soma + parseInt(CPFB.substring(i-1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
+       
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(CPFB.substring(9, 10)) ) 
+        if(document.getElementById("idCPFB").value!=""){
+            return alert("digite um CPFB válido"), document.getElementById("idCPFB").value=""}
+       
+      Soma = 0;
+        for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(CPFB.substring(i-1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+       
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(CPFB.substring(10, 11) ) ){ 
+            if(document.getElementById("idCPFB").value!="") return alert("digite um CPF válido"), document.getElementById("idCPFB").value=""}
+        
+        return true;
+        
+        }
+
+        handleCPFS(e) {
+            const CPFS=e.target.value
+            {console.log(CPFS)}
+            var Soma;
+            var Resto;
+            Soma = 0;
+            if (CPFS == "00000000000") return false;
+             
+            for (let i=1; i<=9; i++) Soma = Soma + parseInt(CPFS.substring(i-1, i)) * (11 - i);
+            Resto = (Soma * 10) % 11;
+           
+            if ((Resto == 10) || (Resto == 11))  Resto = 0;
+            if (Resto != parseInt(CPFS.substring(9, 10)) ) 
+            if(document.getElementById("idCPFS").value!=""){
+                return alert("digite um CPFS válido"), document.getElementById("idCPFS").value=""}
+           
+          Soma = 0;
+            for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(CPFS.substring(i-1, i)) * (12 - i);
+            Resto = (Soma * 10) % 11;
+           
+            if ((Resto == 10) || (Resto == 11))  Resto = 0;
+            if (Resto != parseInt(CPFS.substring(10, 11) ) ){ 
+                if(document.getElementById("idCPFS").value!="") return alert("digite um CPF válido"), document.getElementById("idCPFS").value=""}
+            
+            return true;
+            
+            }
+
+        
+
+
+    handleCEP(e) {
+    //pega CEp
+        const cep=e.target.value;
+        //consultando api
+        ApiCep.SearchCep(cep).then((res) =>{
+            let rua = res.data.logradouro;
+            let bairro = res.data.bairro;
+            let localidade = res.data.localidade;
+            let estado = res.data.uf;
+            //mudando estado
+            this.setState({
+                rua: rua,
+                bairro: bairro,
+                localidade: localidade,
+                estado: estado
+            })
+        })
+
+    }
     handleRegister = () => {
         this.props.history.push('/');
     }
@@ -61,36 +147,38 @@ export default class Register extends Component {
                                 <div className="left-form">
                                     <div>
                                         <label>Nome</label> <br />
-                                        <input placeholder="Ex: Maria" />
+                                        <input type="text"  placeholder="Nome" required={true} />
                                     </div>
 
                                     <div>
                                         <label>E-mail</label> <br />
-                                        <input placeholder="Email" type="email" />
+                                        <input placeholder="Email" type="email" required={true} />
                                     </div>
 
                                     <div>
                                         <label>Senha</label> <br />
-                                        <input placeholder="Senha" type="password" />
+                                        <input placeholder="Senha" type="password" required={true} />
                                     </div>
 
                                     <div>
                                         <label>Confirme sua Senha</label> <br />
-                                        <input placeholder="Senha" type="password" />
+                                        <input placeholder="Senha" type="password" required={true} />
                                     </div>
 
 
                                 </div>
 
                                 <div className="right-form">
+                                    <div className="inputs">
                                     <div>
                                         <label>Sobrenome</label> <br />
-                                        <input placeholder="Ex: Gonçalves" />
+                                        <input placeholder="Sobrenome"  required={true}/>
                                     </div>
-
+ 
                                     <div>
                                         <label>celular</label> <br />
-                                        <input placeholder="(xx)xxxxx-xxxx)" />
+                                        <InputMask mask="(99)99999-9999"  required={true}/>
+                                    </div>
                                     </div>
                                     <div className="checkBoxes">
                                         <div>
@@ -114,49 +202,50 @@ export default class Register extends Component {
                                 <div className="left-form-2">
                                     <div>
                                         <label>CPF</label> <br />
-                                        <input placeholder="xxx-xxx-xxx-xx" />
+                                        <input id="idCPFB" placeholder="xxx-xxx-xxx-xx"  value={this.state.value} onBlur={this.handleCPFB} required={true}/>
                                     </div>
 
                                     <div>
                                         <label>CEP</label> <br />
-                                        <input placeholder="xxxxxx-xxx" />
-                                        <button className="buttonSearchCEPB">
+                                        
+                                        <InputMask mask="99999-999"  type="text" onBlur={this.handleCEP.bind(this)} required={true}/>
+                                        
+                                        <button className="buttonSearchCEPB" onClick={this.handleCEP}>
                                             Buscar
                                         </button>
                                     </div>
 
                                     <div>
                                         <label>Logradouro</label> <br />
-                                        <input placeholder="Avenida dom astolfo pereira" style={{ width: '350px' }} />
+                                        <input placeholder="Logradouro" style={{ width: '350px' }} value={this.state.rua} disabled required={true}/>
                                     </div>
 
                                     <div>
                                         <label>Complemento</label> <br />
-                                        <input placeholder="apartamento 32" style={{ width: '350px' }} />
+                                        <input placeholder="Complemento" style={{ width: '350px' }} />
                                     </div>
                                 </div>
 
                                 <div className="middle-form-2">
                                     <div>
-                                        <label>Número</label> <br />
-                                        <input placeholder="Ex:776" />
-                                    </div>
-
-                                    <div>
                                         <label>Cidade</label> <br />
-                                        <input placeholder="Paranaiba" />
+                                        <input placeholder="Cidade" value={this.state.localidade} disabled required={true}/>
+                                    </div>
+                                    <div>
+                                        <label>Número</label> <br />
+                                        <input placeholder="Número" required={true} />
                                     </div>
                                 </div>
 
                                 <div className="right-form-2">
                                     <div>
                                         <label>Bairro</label> <br />
-                                        <input placeholder="Ex:lapa" />
+                                        <input placeholder="Ex:Bairro" value={this.state.bairro} disabled required={true}/>
                                     </div>
 
                                     <div>
-                                        <label>UF</label> <br />
-                                        <input placeholder="SP" style={{ width: '50px' }} />
+                                        <label>Estado</label> <br />
+                                        <input placeholder="UF" style={{ width: '50px' }} value={this.state.estado} disabled  />
                                     </div>
                                 </div>
                                 <button onClick={this.handleRegister} className="buttonRegister" type="submit">
@@ -174,7 +263,7 @@ export default class Register extends Component {
                             <div className="left-form-3">
                                 <div>
                                     <label>CPF</label> <br />
-                                    <input placeholder="xxx-xxx-xxx-xx" />
+                                    <input id="idCPFS" placeholder="xxx-xxx-xxx-xx"  value={this.state.value} onBlur={this.handleCPFS} required={true}/>
                                 </div>
 
                                 <div>
@@ -184,46 +273,45 @@ export default class Register extends Component {
 
                                 <div>
                                     <label>CEP</label> <br />
-                                    <input placeholder="xxxxxx-xxx" />
-                                </div>
-                                <div>
-                                    <button className="buttonSearchCEPS">
+                                    <InputMask mask="99999-999"  type="text" onBlur={this.handleCEP.bind(this)} required={true}/>
+                                
+                                    <button className="buttonSearchCEPS" onClick={this.handleCEP}>
                                         Buscar
                                     </button>
                                 </div>
 
                                 <div>
                                     <label>Logradouro</label> <br />
-                                    <input placeholder="Avenida dom astolfo pereira" style={{ width: '350px' }} />
+                                    <input placeholder="Logradouro" type="text" style={{ width: '350px' }}  value={this.state.rua} disabled />
                                 </div>
 
                                 <div>
                                     <label>Complemento</label> <br />
-                                    <input placeholder="apartamento 32" style={{ width: '350px' }} />
+                                    <input placeholder="Complemento" type="text" style={{ width: '350px' }} />
                                 </div>
                             </div>
 
                             <div className="middle-form-3">
                                 <div>
                                     <label>Número</label> <br />
-                                    <input placeholder="Ex:776" />
+                                    <input placeholder="Número" type="number" />
                                 </div>
 
                                 <div>
                                     <label>Cidade</label> <br />
-                                    <input placeholder="Paranaiba" />
+                                    <input placeholder="Cidade"  value={this.state.cidade} disabled/>
                                 </div>
                             </div>
 
                             <div className="right-form-3">
                                 <div>
                                     <label>Bairro</label> <br />
-                                    <input placeholder="Ex:lapa" />
+                                    <input placeholder="Bairro"  value={this.state.bairro} disabled />
                                 </div>
 
                                 <div>
-                                    <label>UF</label> <br />
-                                    <input placeholder="SP" style={{ width: '50px' }} />
+                                    <label>Estado</label> <br />
+                                    <input placeholder="UF" style={{ width: '50px' }}  value={this.state.estado} disabled />
                                 </div>
                             </div>
                             <button onClick={this.handleRegister} className="buttonRegister" type="submit">
