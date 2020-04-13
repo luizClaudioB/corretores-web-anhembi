d9d21863b60103aab73b4d8302014138216cddc9
Luiz Claudio Bosco Massarollo Filho <luizclaudioboscomassarollofilho@MacBook-Pro-de-Luiz.local>
2020-04-12 22:44:50 -0300

    first commit search page completed

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -7,9 +7,8 @@ import ReactModalLogin from 'react-modal-login';
 import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types"
 import { Input, PageHeader, Rate } from 'antd';
-import { SearchInput, IconButton, Button, Popover, Menu, Avatar, Combobox,
-    Pane, SideSheet, Heading, Paragraph, Tablist, Tab, Card, Table, TabNavigation} from 'evergreen-ui';
-import { List, ListItem, Badge, Text} from "@chakra-ui/core";
+import { SearchInput, IconButton, Button, Popover, Menu, Avatar, Combobox, Text, Textarea, TagInput,
+    Pane, SideSheet, Heading, Paragraph, Tablist, Tab, Card, Table, TabNavigation, Badge, Label, toaster} from 'evergreen-ui';
 import ReactStarsRating from 'react-awesome-stars-rating';
 
 const facebook = {
@@ -35,6 +34,12 @@ export default class SearchPage extends Component {
       initialTab: null,
       recoverPasswordSuccess: null,
       showSocialMedia: true,
+      selectedIndex: 0,
+      filterList: [],
+      filter: '',
+      value: '',
+      list: [],
+      isaMessage: false,
     };
     }
 
@@ -140,6 +145,27 @@ export default class SearchPage extends Component {
         error: null
       });
     }
+
+    onAddMessage = () => {
+      this.setState({isaMessage: true })
+      this.setState(state => {
+        const list = [...state.list, state.value];
+        return {
+          list,
+          value: '',
+        };
+      });
+    };
+
+    onAddFilter = () => {
+      this.setState(state => {
+        const filterList = [...state.filterList, state.filter];
+        return {
+          filterList,
+          filter: '',
+        };
+      });
+    };
     
     render(){
     const loggedIn = this.state.loggedIn
@@ -176,7 +202,7 @@ export default class SearchPage extends Component {
         </Popover>
         <br />
         <br />
-        <SearchInput marginLeft={360} width={700} height={40} position="fixed" placeholder="Procure um seguro" />
+        <SearchInput top={20} marginLeft={360} width={700} height={40} position="fixed" placeholder="Procure um seguro" />
         {!!loggedIn ? 
         <Popover
           content={
@@ -190,7 +216,7 @@ export default class SearchPage extends Component {
           }
         >
         <Avatar
-          style={{marginLeft: 1320, marginTop: 3, position: 'fixed'}}
+          style={{marginLeft: 1320, marginTop: 15, position: 'fixed'}}
           src="https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg"
           name="Alan Turing"
           size={40}
@@ -198,7 +224,7 @@ export default class SearchPage extends Component {
         </Popover>
         : 
         <Button
-          style={{marginLeft: 1210, marginTop: 4, position: 'fixed'}}
+          style={{marginLeft: 1210, marginTop: 15, position: 'fixed'}}
           className="RML-btn"
           onClick={() => this.openModal('login')}
           appearance="minimal"
@@ -305,26 +331,77 @@ export default class SearchPage extends Component {
         </TabNavigation>
     </div>
     <div>
-    <body style={{marginTop: 100, position: 'fixed'}}>
+    <body style={{position: 'fixed'}}>
     <div>
+    <div style={{marginLeft: 30, marginTop: 100, width: 300, float: 'left', borderRight: '1px solid rgb(214, 206, 200)' }}>
+    <Heading size={700} marginBottom={10}>Filtros</Heading>
+    <TagInput
+      inputProps={{ placeholder: 'Adicione filtros...' }}
+      values={this.state.filterList}
+      onChange={values => {
+        this.setState({ filterList: values })
+      }}
+      style={{marginRight: 4, marginBottom: 30}}
+    />
+    <div> 
+    <Text style={{float: 'left'}} FontFamily='display'>
+    Tipo de seguro
+    </Text>
     <Combobox
         openOnFocus
         items={['Automoveis', 'Aparelhos eletronicos', 'Residencia', 'Motocicletas']}
-        onChange={selected => console.log(selected)}
+        onChange={selected => this.setState({ filter: selected })}
         placeholder="Filtre pelo tipo de seguro"
-        marginBottom={40}
+        marginBottom={30}
+        style={{float: 'left'}}
     />
+    <IconButton marginRight={22} style={{float: 'right'}} onClick={() => {this.onAddFilter()}} icon="plus" />
+    </div> 
+    <div>
+    <Text style={{float: 'left'}} FontFamily='display'>
+    Estado do corretor
+    </Text>
     <Combobox
         openOnFocus
         items={['Sao Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Espirito Santo']}
-        onChange={selected => console.log(selected)}
+        onChange={selected => this.setState({ filter: selected })}
         placeholder="Filtre pelo Estado do corretor"
-        marginBottom={40}
-    />  
+        marginBottom={30}
+        style={{float: 'left'}}
+    />
+    <IconButton marginRight={22} style={{float: 'right'}} onClick={() => {this.onAddFilter()}} icon="plus" />
+    </div>
+    <div style={{float: 'left'}}>
+    <Text style={{float: 'left'}} FontFamily='display'>
+    Empresa
+    </Text>
+    <Combobox
+        openOnFocus
+        items={['Porto Seguro', 'Zurich Seguros', 'Allianz', 'Bradesco Seguros']}
+        onChange={selected => this.setState({ filter: selected })}
+        placeholder="Filtre pelo Estado do corretor"
+        marginBottom={30}
+        style={{float: 'left'}}
+    />
+    </div>
+    <div> 
+    <IconButton marginRight={22} marginTop={20} style={{float: 'right'}} onClick={() => {this.onAddFilter()}} icon="plus" />
+    </div>
+    <div style={{float: 'left'}}>
+    <Text FontFamily='display'>
+    Rating do corretor
+    </Text>  
+    <br />
     <FilterRating></FilterRating>
-    <Table style={{marginLeft: 500}}>
+    </div>
+    </div>
+    <div style={{float:'right'}}>
+    <Heading size={700} marginBottom={10} marginLeft={100} marginTop={100}>Lista de corretores</Heading>
+    <Table style={{marginLeft: 100, border: '1px solid rgb(214, 206, 200)'}}>
     <Table.Head>
-        <Table.SearchHeaderCell />
+        <Table.TextHeaderCell>
+        Nome
+        </Table.TextHeaderCell>
         <Table.TextHeaderCell>
         Tipo
         </Table.TextHeaderCell>
@@ -334,8 +411,11 @@ export default class SearchPage extends Component {
         <Table.TextHeaderCell>
         Empresa
         </Table.TextHeaderCell>
+        <Table.TextHeaderCell>
+        Estado
+        </Table.TextHeaderCell>
         </Table.Head>
-        <Table.Body height={240} width={700}>
+        <Table.Body height={240} width={800}>
         <Table.Row isSelectable onSelect={() => this.setState({ isShown: true })}>
             <Table.TextCell>Carlos Alberto</Table.TextCell>
             <Table.TextCell>Seguro de Automoveis</Table.TextCell>
@@ -343,6 +423,7 @@ export default class SearchPage extends Component {
             <VendorRating></VendorRating>
             </Table.TextCell>
             <Table.TextCell>Porto Seguro</Table.TextCell>
+            <Table.TextCell>Sao Paulo</Table.TextCell>
         </Table.Row>
         <Table.Row isSelectable onSelect={() => this.setState({ isShown: true })}>
             <Table.TextCell>Silvia Machado</Table.TextCell>
@@ -351,9 +432,11 @@ export default class SearchPage extends Component {
             <VendorRating></VendorRating>
             </Table.TextCell>
             <Table.TextCell>Zurich Seguros</Table.TextCell>
+            <Table.TextCell>Minas Gerais</Table.TextCell>
         </Table.Row>
     </Table.Body>
     </Table>
+    </div>
     </div>
     <div>
     <React.Fragment>
@@ -368,14 +451,17 @@ export default class SearchPage extends Component {
       >
         <Pane zIndex={1} flexShrink={0} elevation={0} backgroundColor="white">
           <Pane padding={16} borderBottom="muted">
-            <Heading size={600}>Carlos Alberto</Heading>
+            <Heading size={600}>
+            Carlos Alberto
+            <Badge color="neutral" marginBottom={1} marginLeft={2} marginRight={8}>Corretor Premium</Badge>
+            </Heading>
             <Paragraph size={400} color="muted">
               Informacoes sobre o(a) corretor(a)
             </Paragraph>
           </Pane>
           <Pane display="flex" padding={8}>
             <Tablist>
-               {['Perfil do corretor'].map(
+               {['Perfil', 'Contato'].map(
                   (tab, index) => (
                     <Tab
                       key={tab}
@@ -390,23 +476,105 @@ export default class SearchPage extends Component {
             </Tablist>
           </Pane>
         </Pane>
+        {this.state.selectedIndex === 0 ?
         <Pane flex="1" overflowY="scroll" background="tint1" padding={16}>
           <Card
             backgroundColor="white"
             elevation={0}
-            height={240}
+            height={100}
             display="flex"
             alignItems="center"
             justifyContent="center"
           >
-            <Heading>
-            <Card alignItems="center"><Text>Avaliacao do corretor: </Text>
-            <VendorRating>
-            </VendorRating>
-            </Card>
-            </Heading>
+            <div>
+            <p>
+            <Text>Tipo:   </Text>
+            <Text>Seguro de Automoveis</Text>
+            </p>
+            <p>
+            <Text>Empresa:   </Text>
+            <Text>Porto Seguro</Text>
+            </p>
+            <p>
+            <Text>Estado:   </Text>
+            <Text>Sao Paulo</Text>
+            </p>
+            </div>
           </Card>
+          <br />
+          <Label
+            htmlFor="textarea-2"
+            marginBottom={4}
+            display="block"
+          >
+            <br />
+            Ultimos comentarios sobre este corretor:
+          </Label>
+          <Textarea
+            appearance='minimal'
+            value={this.state.list[0]}
+            disabled={true}
+            style={{resize: 'none'}}
+          /> 
+          <br />
+          <br />
+          <Textarea
+            value={this.state.list[1]}
+            disabled={true}
+            style={{resize: 'none'}}
+          />
+          <br />
+          <br />
+          <Textarea
+            value={this.state.list[2]}
+            disabled={true}
+            style={{resize: 'none'}}
+          />
         </Pane>
+        : <Pane flex="1" overflowY="scroll" background="tint1" padding={16}>
+        <Card
+          backgroundColor="white"
+          elevation={0}
+          height={100}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <div>
+          <p>
+          <Text>Telefone celular: </Text>
+          <Text>(11) 98765-4321</Text>
+          </p>
+          <p>
+          <Text>Correio eletronico: </Text>
+          <Text>carlosalberto@portoseguro.br</Text>
+          </p>
+          <p>
+          <Text>Disponibilidade: </Text>
+          <Text>Segunda a sexta das 10h as 18h</Text>
+          </p>
+          </div>
+          </Card>
+          <Pane>
+          <Label
+            htmlFor="textarea-2"
+            marginBottom={4}
+            display="block"
+          >
+            <br />
+            Deixe uma mensagem para o corretor
+          </Label>
+          <Textarea
+            required
+            onChange={e => this.setState({ value: e.target.value })}
+            value={this.state.value}
+            placeholder="Escreva aqui sua mensagem..."
+            style={{resize: 'none'}}
+          />
+          <Button style={{marginLeft: 520, marginTop: 1}} onClick={() => {this.onAddMessage(); 
+            toaster.success('Sua mensagem foi enviada!', {duration: 4})}}>Enviar</Button>
+        </Pane>
+        </Pane> }
       </SideSheet>
     </React.Fragment>   
     </div>
